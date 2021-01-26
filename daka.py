d78001f8cd913aec2df8fde6da4636aaefffd75e
admin <1160410692@qq.com>
2021-01-26 14:08:34 +0800

    更新打卡脚本

--- daka.py
+++ daka.py
@@ -10,9 +10,11 @@ def send_message(message):
     # 初始化机器人
     xiaoding = DingtalkChatbot(webhook)
     # 当前时间
-    now_time = time.strftime('%Y-%m-%d %H-%M-%S', time.localtime())# time.localtime()：计算机当地时间
-    now_time = f"\n当前时间：{now_time}"
+    now_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())  # time.localtime()：计算机当地时间
     # Test消息@所有人
-    xiaoding.send_text(msg = '滴滴滴：' + message + now_time, is_at_all = False)
+    xiaoding.send_text(msg=f"滴滴滴：{message} \n当前时间：{now_time}", is_at_all=False)  # "滴滴滴："是钉钉的安全设置的自定义关键词
 
-send_message('打卡！！！想被扣钱吗!!')
+    # xiaoding.send_text(msg = '滴滴滴：test' + message + now_time, is_at_all = False)
+
+
+send_message('打卡啊！！！迟到一分钟一块钱啊!!!')
